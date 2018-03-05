0ca59f13d38b57ae63d46b104a52b342631e6815
nekekii <icyduckblock@gmail.com>
2018-03-04 18:52:29 -0700

    Change site for Kunai
    Add images, change links, change style

--- js/pagetransition.js
+++ js/pagetransition.js
@@ -2,18 +2,19 @@ $(document).ready(function(){
     console.log("Welcome to my JS, brave traveler.");
     console.log("Don't ask.");
     console.log("this is cool I guess...");
-    $("#social").html('<img src="images/steam.png" id="steam"><img src="images/twitter.png" id="tw"><img src="images/github.png" id="gh">');
+    console.log("Made with love for KunaiMC by nekekii <3");
+    $("#social").html('<img src="images/youtube.png" id="youtube"><img src="images/twitter.png" id="tw"><img src="images/namemc.jpg" id="mc"><img src="images/osu.png" id="osu"><img src="images/discord.png" id="discord">');
     $("#text").css("top", "-100vh");
     $("#text").animate({top: '25vh'}, 1000);
 
-    $("#steam").click(function(){
+    $("#youtube").click(function(){
         //disable for mobile
         if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-          window.location.href = "http://steamcommunity.com/profiles/76561198071986407";
+          window.location.href = "https://www.youtube.com/c/KunaiMC";
         }else{
           $("#text").animate({left: '-100vw'}, 1000, function(){
-            console.log("Have a nice day! See you on steam.");
-            window.location.href = "http://steamcommunity.com/profiles/76561198071986407";
+            console.log("Have a nice day! See you on YouTube.");
+            window.location.href = "https://www.youtube.com/c/KunaiMC";
           });
         }
     });
@@ -21,23 +22,47 @@ $(document).ready(function(){
     $("#tw").click(function(){
         //disable for mobile
         if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-          window.location.href = "https://twitter.com/nekekiii";
+          window.location.href = "https://twitter.com/kunaiyt";
         }else{
           $("#text").animate({top: '100vw'}, 1000, function(){
             console.log("Have a nice day! See you on Twitter.");
-            window.location.href = "https://twitter.com/nekekiii";
+            window.location.href = "https://twitter.com/kunaiyt";
           });
         }
     });
 
-    $("#gh").click(function(){
+    $("#mc").click(function(){
         //disable for mobile
         if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-          window.location.href = "https://github.com/nekekii/";
+          window.location.href = "https://namemc.com/profile/Koyano";
+        }else{
+          $("#text").animate({top: '100vw'}, 1000, function(){
+            console.log("Have a nice day! See you on Twitter.");
+            window.location.href = "https://namemc.com/profile/Koyano";
+          });
+        }
+    });
+
+    $("#osu").click(function(){
+        //disable for mobile
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+          window.location.href = "https://osu.ppy.sh/u/Kunay";
+        }else{
+          $("#text").animate({top: '100vw'}, 1000, function(){
+            console.log("Have a nice day! See you on Twitter.");
+            window.location.href = "https://osu.ppy.sh/u/Kunay";
+          });
+        }
+    });
+
+    $("#discord").click(function(){
+        //disable for mobile
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+          window.location.href = "https://discord.gg/DS8HTn6";
         }else{
           $("#text").animate({left: '100vw'}, 1000, function(){
-            console.log("Have a nice day! See you on GitHub.");
-            window.location.href = "https://github.com/nekekii/";
+            console.log("Have a nice day! See you on Discord.");
+            window.location.href = "https://discord.gg/DS8HTn6";
           });
         }
     });
